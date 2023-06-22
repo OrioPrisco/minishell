ad6a723dc6bb284a7ff53b8a8428074e40d24b30
Drew Pentland <drewtpentland@gmail.com>
2023-06-22 18:04:28 +0200

    added error function
    added a file with an error function as well as added basic functionality for readline.

--- srcs/ms_prompt_utils.c
+++ srcs/ms_prompt_utils.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 16:38:29 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/06/22 17:42:20 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/06/22 18:03:21 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,11 @@ int	prompt_loop(char **env)
 	{
 	}
 	str_input = 0;
-	readline(str_input);
+	str_input = (char *)malloc(100 * sizeof(char));
+	if (!str_input)
+		ms_error("malloc");
+	str_input = readline("> ");
 	ft_printf("%s\n", str_input);
+	free(str_input);
 	return (0);
 }
