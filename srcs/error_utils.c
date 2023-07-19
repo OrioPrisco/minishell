bec4beb240827c86c3f2b4e612d35965814b452e
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-07-19 15:40:22 +0200

    Fix memory issue in msh_exit
    created a function wich free a pointer to a string.
    passing free to vector_free causes the vector->data to get freed, and then
    memory errors.
    
    fixes #21

--- srcs/error_utils.c
+++ srcs/error_utils.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:58:47 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/07/18 17:01:49 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2023/07/19 15:37:46 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include "libft.h"
 #include <stdio.h>
+#include "utils.h"
 
 void	msh_error(const char *message)
 {
@@ -28,7 +29,7 @@ void	msh_error(const char *message)
 void	msh_exit(char **env, t_vector *com_list)
 {
 	save_history(env, com_list);
-	vector_free(com_list, free);
+	vector_free(com_list, free_str);
 	ft_putstr_fd("exit\n", 1);
 	exit(EXIT_SUCCESS);
 }
