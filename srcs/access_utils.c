1ba19125bc3c5b42f86f4d693762d32c722a5627
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-08-06 23:45:25 +0200

    access_loop : chesck access one at a time
    isntead of appending the command name to every element in path,
    just concatenate one item and check using path_concat
    and then check for access.
    This uses less memory, but other memory optimizations are still possible,
    like using stringviews into the PATH variable, (that would require a split_to_strview function)
    or reusing a character buffer to avoid malloc and freeing strings over and over (this would require path_concat to work with a character buffer like a vector)

--- srcs/access_utils.c
+++ srcs/access_utils.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:00:37 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/08/06 22:34:40 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2023/08/06 23:44:38 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,53 +22,6 @@
 #include "path.h"
 
 /*
-**	add_command_to_path
-**		Adds the command from the single_command function to the end of each 
-		item in the path variable so it can be run through the access function.
-*/
-
-bool	add_command_to_path(char *command, char **path)
-{
-	int		i;
-
-	i = 0;
-	while (path[i])
-	{
-		path[i] = path_concat_free(path[i], command, FREE_ALWAYS, FREE_NEVER);
-		if (!path[i])
-			return (1);
-	}
-	return (0);
-}
-
-/*
-**	NAME
-		*get_path_with_commands
-**	DESCRIPTION
-		Gets path from environment variables, splits by ':', then takes the table from 
-		ft_split and adds 'command' or '/command' to the end.
-**	RETURN
-		Returns a freeable table with command added to the end of each item in PATH.
-		Returns NULL on error.
-*/
-
-char	**get_path_with_commands(char *command, char **envp)
-{
-	const char	*env;
-	char		**path;
-
-	env = get_env_var(envp, "PATH", 4);
-	if (!env)
-		return (0);
-	path = ft_split(env, ':');
-	if (!path)
-		return (0);
-	if (add_command_to_path(command, path))
-		return (table_free(path), NULL);
-	return (path);
-}
-
-/*
 **	NAME
 		*get_process_name
 **	DESCRIPTION
@@ -76,31 +29,31 @@ char	**get_path_with_commands(char *command, char **envp)
 		jumping over any other token (ie. T_REDIRECT_STDIN, T_REDIRECT_APPEND_STDIN,
 		T_REDIRECT_APPEND_STDOUT, T_HEREDOC)
 **	RETURN
-		Returns a malloced string containing the command.
+		Returns a non malloced string containing the command.
 		Returns an empty string if no command is found. (ie. '<< heredoc' is a valid
 		command)
 		Returns NULL if malloc error.
 */
 
-char	*get_process_name(t_owned_token *token)
+static const char	*get_process_name(t_owned_token *token)
 {
 	while (token->type != T_END && token->type != T_PIPE)
 	{
 		if (is_redirect_type(token->type))
 			token ++;
 		else if (token->type == T_STR)
-			return (ft_strdup(token->str));
+			return (token->str);
 		token++;
 	}
-	return (ft_strdup(""));
+	return ("");
 }
 
 /*
 **	NAME
 		check_access
 **	DESCRIPTION
-		Takes a path (with commands appended to the end of each member) and
-		iterates over table searching for access.
+		Takes a command and a path,
+		and iterates over table searching for access.
 **	RETURN
 		Returns malloced string containing the abs path and command if one is 
 		found in path if access is found.
@@ -108,16 +61,19 @@ char	*get_process_name(t_owned_token *token)
 		Returns NULL if malloc error.
 */
 
-char	*check_access(char **path)
+static char	*check_access(const char *const *path, const char *command)
 {
-	int		i;
+	char	*temp;
 
-	i = 0;
-	while (path[i])
+	while (*path)
 	{
-		if (access(path[i], F_OK | X_OK) == 0)
-			return (ft_strdup(path[i]));
-		i++;
+		temp = path_concat(*path, command);
+		if (!temp)
+			return (NULL);
+		if (!access(temp, F_OK | X_OK))
+			return (temp);
+		free(temp);
+		path++;
 	}
 	return (ft_strdup(""));
 }
@@ -139,19 +95,23 @@ char	*check_access(char **path)
 
 char	*access_loop(t_owned_token *tokens, char **envp)
 {
-	char		**path;
-	char		*command;
+	char		**path_tab;
+	const char	*path;
+	const char	*command;
+	char		*command_path;
 
+	path = get_env_var(envp, "PATH", ft_strlen("PATH"));
+	if (!path)
+		return (0);
 	command = get_process_name(tokens);
 	if (!command)
 		return (0);
 	if (!command[0])
-		return (command);
-	path = get_path_with_commands(command, envp);
-	free(command);
-	if (!path)
+		return (ft_strdup(command));
+	path_tab = ft_split(path, ':');
+	if (!path_tab)
 		return (0);
-	command = check_access(path);
-	table_free(path);
-	return (command);
+	command_path = check_access((const char **)path_tab, command);
+	table_free(path_tab);
+	return (command_path);
 }
