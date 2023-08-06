24533c8478e9324b5b654600e4c0103a886f8ea6
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-08-06 22:27:08 +0200

    path.h : add FREE_NEVER
    for when the path shouldn't be freed

--- includes/path.h
+++ includes/path.h
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 20:01:38 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2023/08/06 20:09:35 by OrioPrisco       ###   ########.fr       */
+/*   Updated: 2023/08/06 22:26:58 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PATH_H
 
 typedef enum e_free_mode {
+	FREE_NEVER = 0,
 	FREE_ON_ERROR = 1 << 0,
 	FREE_ON_SUCCESS = 1 << 1,
 	FREE_ALWAYS = FREE_ON_ERROR | FREE_ON_SUCCESS,
