f353961bb970caec1af482737f18307979284c4c
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-08-08 16:13:22 +0200

    add is_directory util function

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,12 +6,13 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 15:34:07 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2023/08/03 09:56:13 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/08/08 16:10:40 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 #include "libft.h"
+#include <sys/stat.h>
 
 void	free_str(void *data)
 {
@@ -40,4 +41,17 @@ void	table_free(char **table)
 	}
 	free(table);
 	return ;
+}
+
+//returns
+// -1 on error, and errno gets set
+// 0 if filename is not a directory
+// 1 if filename is a directory
+int	is_directory(const char *filename)
+{
+	struct stat	file_stat;
+
+	if (stat(filename, &file_stat))
+		return (-1);
+	return (S_ISDIR(file_stat.st_mode));
 }
