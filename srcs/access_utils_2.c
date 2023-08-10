70dbc8776d0c0bf7ec0afcc62e0143a4455f3d3d
Drew Pentland <dpentlan@student.42.fr>
2023-08-10 10:23:47 +0200

    Adding better Error messages for access loop failure.

--- srcs/access_utils_2.c
+++ srcs/access_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 13:02:35 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/08/08 16:53:55 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/08/10 10:22:07 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 #include "libft.h"
 #include "ft_printf.h"
+#include "utils.h"
+#include <unistd.h>
 
 void	print_access_debug(char *execve_command)
 {
@@ -34,5 +36,10 @@ void	print_access_debug(char *execve_command)
 
 void	access_error_print(const char *exec_name)
 {
-	ft_dprintf(2, "%s: command not found\n", exec_name);
+	if (is_directory(exec_name))
+		ft_dprintf(2, "%s: Is a directory\n", exec_name);
+	else if (access(exec_name, F_OK | X_OK))
+		ft_dprintf(2, "%s: Permission denied\n", exec_name);
+	else
+		ft_dprintf(2, "%s: command not found\n", exec_name);
 }
