6bb1f43e4f5659c3d8bfee36c1a79f2b66c85a17
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-08-14 18:07:16 +0200

    Add WIP sort_wildcards
    can't use strcoll so i'll have to make a scuffed version
    use strcmp for now as a temporary measure

--- srcs/wildcards2.c
+++ srcs/wildcards2.c
@@ -1,24 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.h                                            :+:      :+:    :+:   */
+/*   wildcards2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/19 15:35:18 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2023/09/08 03:38:46 by OrioPrisco       ###   ########.fr       */
+/*   Created: 2023/08/13 18:43:42 by OrioPrisco        #+#    #+#             */
+/*   Updated: 2023/08/15 21:47:59 by OrioPrisco       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_H
-# define UTILS_H
-# include "vector.h"
+#include "wildcards.h"
+#include "vector.h"
+#include "utils.h"
+#include "tokens.h"
 
-// for vector_free
-void	free_str(void *data);
-void	table_free(char **table);
-void	table_print(char **table);
-// for vector comparisons
-int		compare_str(void *a, void *b);
-
-#endif
+void	sort_wildcards(t_vector *vector)
+{
+	vector_sort(vector, compare_str);
+}
