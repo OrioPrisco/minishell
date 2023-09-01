d30914b5d705234f9b3227265c8d22918a10cfd4
Drew Pentland <dpentlan@student.42.fr>
2023-09-01 15:27:07 +0200

    fixed memory leaks
    fixed 2 memory leaks from readline and from left over tables if a built in is called.

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,12 +6,14 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 13:33:20 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/01 15:07:41 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/01 15:22:18 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "ft_printf.h"
+#include "utils.h"
+#include <stdlib.h>
 
 /*
 	NAME
@@ -20,6 +22,10 @@
 		
 	RETURN
 		
+	NOTE
+		Since execve is not being called, any leaks that happen will show up in
+		valgrind.
+		Make sure you check each builtin extensively for leaks.
 */
 
 void	builtin_commands(char *execve_command, char **execve_com_args,
@@ -34,5 +40,7 @@ void	builtin_commands(char *execve_command, char **execve_com_args,
 	if (envp)
 	{
 	}
+	table_free(execve_com_args);
+	free(execve_command);
 	ft_printf("You entered a builtin!\n");
 }
