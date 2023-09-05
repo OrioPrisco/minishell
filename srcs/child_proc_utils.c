def4ab5e6188b34e39117ec1be1eefbe254eedef
Drew Pentland <dpentlan@student.42.fr>
2023-09-05 10:28:11 +0200

    two small fixes
    fixed issue where stdin redirects did not work with pipes. pipe duping needs to happen before stdin redirect to maintain bash like behavior. Also added conditional so that pipe duping only happens if a pipe is present. does this by checking the pipeinfo.

--- srcs/child_proc_utils.c
+++ srcs/child_proc_utils.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 16:57:40 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/04 16:56:46 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/05 10:24:41 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,7 @@ int	pipe_dups(t_com_segment *com_seg, t_pipe_info *pipeinfo)
 		print_here_doc_contents(here_doc_contents);
 	//print_open_redirects((t_fds *)vec_fds.data, vec_fds.size);
 	//table_print(execve_com_args);
+	//ft_printf("pipeinfo: %d %d %d\n", pipeinfo->pipefd[0], pipeinfo->pipefd[1], pipeinfo->old_pipe);
 **/
 
 void	single_command(t_com_segment com_seg, t_cominfo *cominfo,
@@ -78,11 +79,12 @@ void	single_command(t_com_segment com_seg, t_cominfo *cominfo,
 	int			ret;
 
 	vector_init(&vec_fds, sizeof(t_fds));
+	if (pipeinfo->pipefd[0] != 0 && pipeinfo->pipefd[1] != 0)
+		pipe_dups(&com_seg, pipeinfo);
 	ret = check_and_open_redirects(com_seg.tokens, &vec_fds, com_seg.start,
 			com_seg.stop);
 	if (ret)
 		msh_exit_child(cominfo->com_list);
-	pipe_dups(&com_seg, pipeinfo);
 	redir_stdout_and_clean(&vec_fds);
 	exec_command(cominfo, com_seg, &vec_fds);
 	msh_exit_child(cominfo->com_list);
