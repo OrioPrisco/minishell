1be20d92f97debfb25f4fd5e473c4d1a6824e97c
Drew Pentland <dpentlan@student.42.fr>
2023-09-08 15:03:48 +0200

    remove useless line

--- srcs/fd_utils_3.c
+++ srcs/fd_utils_3.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:54:41 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/08 14:59:59 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/08 15:03:34 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 int	cleanup_pipes(t_pipe_info *pipeinfo)
 {
-	(void) pipeinfo;
 	if (pipeinfo->pipefd[0])
 		close (pipeinfo->pipefd[0]);
 	if (pipeinfo->pipefd[1])
