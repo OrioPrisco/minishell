247d0156aa0939e23ecad0b7fd6c2a710a7e1e1c
Drew Pentland <dpentlan@student.42.fr>
2023-09-08 15:01:05 +0200

    fixes issue where running ./minishell inside of ./minishell causes weird behavior with fds.

--- srcs/fd_utils_3.c
+++ srcs/fd_utils_3.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:54:41 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/07 14:57:26 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/08 14:59:59 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 int	cleanup_pipes(t_pipe_info *pipeinfo)
 {
 	(void) pipeinfo;
-	close (pipeinfo->pipefd[0]);
-	close (pipeinfo->pipefd[1]);
+	if (pipeinfo->pipefd[0])
+		close (pipeinfo->pipefd[0]);
+	if (pipeinfo->pipefd[1])
+		close (pipeinfo->pipefd[1]);
 	return (0);
 }
