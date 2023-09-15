948ec5d1af06efe272a1d91522cbd43dacdd3aaa
Drew <91377105+Drewski6@users.noreply.github.com>
2023-09-15 12:32:52 +0200

    Fixing error messages
    changing how some returns are checked in access_error_print

--- srcs/access_utils_2.c
+++ srcs/access_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 13:02:35 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/14 12:36:15 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/15 12:42:17 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include "path.h"
 #include <unistd.h>
 #include "vector.h"
+#include <errno.h>
 
 void	print_access_debug(char *execve_command)
 {
@@ -37,10 +38,19 @@ void	print_access_debug(char *execve_command)
 
 void	access_error_print(const char *exec_name)
 {
-	if (is_directory(exec_name))
-		ft_dprintf(2, "%s: Is a directory\n", exec_name);
-	else if (access(exec_name, F_OK | X_OK))
-		ft_dprintf(2, "%s: Permission denied\n", exec_name);
+	int	ret;
+
+	if (ft_strchr(exec_name, '/'))
+	{
+		ret = is_directory(exec_name);
+		if (ret == 1)
+			ft_dprintf(2, "%s: Is a directory\n", exec_name);
+		else if (ret == -1)
+			msh_error(exec_name);
+		else if (access(exec_name, F_OK | X_OK))
+			msh_error(exec_name);
+			//ft_dprintf(2, "%s: Permission denied\n", exec_name);
+	}
 	else
 		ft_dprintf(2, "%s: command not found\n", exec_name);
 }
