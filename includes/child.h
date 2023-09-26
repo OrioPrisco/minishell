e88da6b9cda1af92f9934695c0b65d696b5b4298
Drew <91377105+Drewski6@users.noreply.github.com>
2023-09-26 17:09:22 +0200

    Added redirects to prefork builtins

--- includes/child.h
+++ includes/child.h
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 16:58:59 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/22 14:36:28 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:27:00 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 
 void	single_command(t_com_segment com_seg, t_cominfo *cominfo,
 			t_pipe_info *pipeinfo);
+void	open_heredocs(t_owned_token *tok, size_t start, size_t stop);
 
 #endif
