f27f5d7f57b9a41fb33fd396d5aec5b1274e7d26
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-09-26 16:10:49 +0200

    redirects : remove extra check for stdout
    no need to check that a file exists to redirect stdout

--- srcs/redirects.c
+++ srcs/redirects.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 12:21:36 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/09/26 15:42:58 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/09/26 15:58:56 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,8 @@ static int	redir_stdout_token_found(t_owned_token *owned_token,
 {
 	int		fd;
 
+	if (is_directory(fn_start) == 1)
+		return (redirect_error_print(fn_start), -1);
 	if (owned_token->type == T_REDIRECT_STDOUT)
 		fd = open(fn_start, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 	else
@@ -105,20 +107,21 @@ int	open_redirects(t_vector *tokens, int start, int stop, t_vector *vec_fds)
 	ret = 0;
 	while (i < stop)
 	{
-		current = (t_owned_token *)tokens->data + i;
+		current = (t_owned_token *)tokens->data + i++;
 		if (is_redirect_type(current->type) && current->type != T_HEREDOC)
 		{
-			if (is_directory(current[1].str))
-				return (redirect_error_print(current[1].str), 1);
 			if (current->type == T_REDIRECT_STDIN)
+			{
+				if (is_directory(current[1].str))
+					return (redirect_error_print(current[1].str), 1);
 				ret = redir_stdin_token_found(current[1].str);
+			}
 			else
 				ret = redir_stdout_token_found(current, vec_fds,
 						(current + 1)->str);
 			if (ret)
 				return (ret);
 		}
-		i++;
 	}
 	return (0);
 }
