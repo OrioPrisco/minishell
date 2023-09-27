98d867b8ff304ea2c67246e92645f13f273ecc8b
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-09-27 14:52:22 +0200

    Fix parser not erroring on nothing being piped
    `|foo` now errors
    `foo || bar ` now errors

--- srcs/parser/parse_pipe.c
+++ srcs/parser/parse_pipe.c
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users.nor  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 17:15:56 by OrioPrisco        #+#    #+#             */
-/*   Updated: 2023/09/18 13:15:03 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/09/27 14:53:36 by OrioPrisc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int	parse_pipe(t_vector *dest, const t_token *tok)
 {
 	t_owned_token	token;
 
+	if (dest->size == 0 || tok[1].type == T_PIPE
+		|| (tok[1].type == T_SPACE && tok[2].type == T_PIPE))
+		return (ft_dprintf(2, "Parse error near |\n"), -1);
 	token = (t_owned_token){NULL, T_PIPE, 0};
 	if (tok[1].type == T_END
 		|| (tok[1].type == T_SPACE && tok[2].type == T_END))
